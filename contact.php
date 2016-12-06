363c789555b90a038f19985c3c3692d68c1fdf01
Jose DeLavalle <me@slickwebstudio.com>
2016-12-06 16:45:19 -0500

    google analytics

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 if($_POST)
 {
-    $to_email       = "me@slickwebstudio.com"; //Recipient email, Replace with own email here
+    $to_email       = "me@josedelavalle.com"; //Recipient email, Replace with own email here
     
     //check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
